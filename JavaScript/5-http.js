1143e708a086c12fbdbfc937fe56d8acef356c9b
tshemsedinov <timur.shemsedinov@gmail.com>
2017-08-26 15:15:53 +0300

    Add http server

--- JavaScript/5-http.js
+++ JavaScript/5-http.js
@@ -4,23 +4,24 @@ const fs = require('fs');
 const zlib = require('zlib');
 const http = require('http');
 
-const rs = fs.createReadStream('1-readable.js');
+const rs = fs.createReadStream('index.html');
 const gs = zlib.createGzip();
 
-var buffers = [];
+const buffers = [];
 let buffer;
 
-gs.on('data', function(buffer) {
+gs.on('data', (buffer) => {
   buffers.push(buffer);
 });
 
-gs.on('end', function() {
+gs.on('end', () => {
   buffer = Buffer.concat(buffers);
 });
 
 rs.pipe(gs);
 
 const server = http.createServer((request, response) => {
+  console.log(request.url);
   response.writeHead(200, { 'Content-Encoding': 'gzip' });
   response.end(buffer);
 });
