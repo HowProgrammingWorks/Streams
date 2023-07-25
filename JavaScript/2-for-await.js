68bbe015ff28a69d9a855d14efc2c2bf8d88d9fe
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2023-07-25 20:25:59 +0300

    Fix examples

--- JavaScript/2-for-await.js
+++ JavaScript/2-for-await.js
@@ -8,9 +8,9 @@ const readable = new stream.Readable();
 // Read style: AsyncIterable
 
 readable.push('Hello ');
+//readable.emit('error', new Error('Cant generate data'));
 readable.push('World!');
 readable.push(null);
-//readable.emit('error', new Error('Cant generate data'));
 
 const main = async () => {
   for await (const chunk of readable) {
--- JavaScript/6-timeout.js
+++ JavaScript/6-timeout.js
@@ -8,7 +8,7 @@ const main = async () => {
   const readable = fs.createReadStream('data.tmp');
   const writable = fs.createWriteStream('data.gz');
   const gzip = zlib.createGzip();
-  const options = { signal: AbortSignal.timeout(20) };
+  const options = { signal: AbortSignal.timeout(10) };
   await streams.pipeline(readable, gzip, writable, options);
   console.log('Done');
 };
